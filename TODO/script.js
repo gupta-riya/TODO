967744f3bfb0a56ff90eb03e60314d7f0687272d
gupta-riya <reeya211999@gmail.com>
2021-04-27 23:11:51 +0530

    change in events and styling

--- TODO/script.js
+++ TODO/script.js
@@ -3,34 +3,44 @@ let colorBtn = document.querySelectorAll(".filter");
 let mainCont = document.querySelector(".main-container");
 let body = document.body;
 let plusButton = document.querySelector(".fa-plus");
+let availColors = [];
 
+for (let i = 0; i < colorBtn.length; i++) {
+    let color = colorBtn[i].children[0].classList[1];
+    availColors.push(color);
+}
 
-for (let i = 0 ; i < colorBtn.length ; i++)
-{
-    colorBtn[i].addEventListener("click", function(e){
+for (let i = 0; i < colorBtn.length; i++) {
+    colorBtn[i].addEventListener("click", function (e) {
 
         // class list will give all the classes associated with the element
         //console.log(colorBtn[i].classList[1]);
         let color = colorBtn[i].children[0].classList[1];
-        mainCont.style.backgroundColor = color;
+
+
+        mainCont.setAttribute("style", "background-color: " + color + " ;");
+
 
     })
 }
 
 
-plusButton.addEventListener("click",function(){
+plusButton.addEventListener("click", function () {
 
-    createModal();
-    addModalEvents();
- 
+    let modalexist = document.querySelectorAll(".modal_container")
+    if (modalexist.length == 0) {
+        createModal();
+        addModalEvents();
+
+    }
+
 })
 
 
-function createModal()
-{
-    
+function createModal() {
+
     let modal_container = document.createElement("div");
-    modal_container.setAttribute("class","modal_container");
+    modal_container.setAttribute("class", "modal_container");
     modal_container.innerHTML = `<div class = "input_container">
     <textarea class="modal_input" placeholder="Enter Your Task Here"></textarea>
     </div>
@@ -48,54 +58,69 @@ function createModal()
     body.appendChild(modal_container);
 }
 
-function addModalEvents()
-{
+function addModalEvents() {
     let colorList = document.querySelectorAll(".modal_filter_container .filter_task");
     let selectedColor = "black";
 
-    colorList[0].classList.add("border");  
-    for(let i = 0 ; i < colorList.length ; i++)
-    {
-        colorList[i].addEventListener("click", function(e){
+    colorList[3].classList.add("border");
+    for (let i = 0; i < colorList.length; i++) {
+        colorList[i].addEventListener("click", function (e) {
 
-          colorList.forEach((filter)=>{
-              filter.classList.remove("border");
-          })
-           
-           colorList[i].classList.add("border");   
-           selectedColor = colorList[i].classList[1];        
+            colorList.forEach((filter) => {
+                filter.classList.remove("border");
+            })
+
+            colorList[i].classList.add("border");
+            selectedColor = colorList[i].classList[1];
 
         })
     }
 
     let input = document.querySelector(".modal_input");
 
-    input.addEventListener("keydown", function (e){
+    input.addEventListener("keydown", function (e) {
 
-        if (e.key == "Enter"){
+        if (e.key == "Enter") {
 
             var inputText = input.value;
+            inputText = inputText.trim();
             document.querySelector(".modal_container").remove();
-            addTask(selectedColor,inputText);
-            
+            if (inputText.length > 0) {
+                addTask(selectedColor, inputText);
+                addTaskEvent();
+
+            }
 
         }
     })
 }
 
-function addTask(color,text)
-{
+function addTask(color, text) {
     let taskCont = document.createElement("div");
-    taskCont.setAttribute("class","task_container");
-    taskCont.innerHTML = `<div class="task_container ">
+    taskCont.setAttribute("class", "task_container");
+    taskCont.innerHTML = `
     <div class="task_header_color ${color}"></div>
     <div class="task_body">
-        <div class="uid">#Example</div>
+        <div class="uid">#exampleId</div>
         <div class="task_desc">${text}</div>
     </div>
-    </div>
+    
     `
 
     document.querySelector(".main-container").appendChild(taskCont);
 
+}
+
+function addTaskEvent() {
+    let taskcontainer = document.querySelectorAll(".task_container");
+    let taskHeader = document.querySelectorAll(".task_header_color");
+    for (let i = 0; i < taskcontainer.length; i++) {
+        taskHeader[i].addEventListener("click", function () {
+
+            let currcolor = taskcontainer[i].children[0].classList.item(1);
+            taskcontainer[i].children[0].classList.remove(currcolor);
+            let randomIndex = Math.floor(Math.random() * 10) % 4;
+            taskcontainer[i].children[0].classList.add(availColors[randomIndex]);
+        })
+    }
 }
